66ada387fa5dd41997902b2fe35942d0b5eebbfd
Danilo Gonçalves <145388263+Danilo-gn@users.noreply.github.com>
2025-03-31 10:51:39 -0300

    update de homepage

--- src/App.tsx
+++ src/App.tsx
@@ -60,18 +60,38 @@ function App() {
     return `${bundleName} ${weaponName}`;
   };
 
-  
-  const randomIndex = Math.floor(Math.random() * bundles.length);
-  const randomItem = bundles[randomIndex];
-  
-    
+  const backgrounds: any[] = ['https://raw.githubusercontent.com/Danilo-gn/Skin-Manager/refs/heads/main/public/background1.jpg', 'https://raw.githubusercontent.com/Danilo-gn/Skin-Manager/refs/heads/main/public/background2.jpg', 'https://raw.githubusercontent.com/Danilo-gn/Skin-Manager/refs/heads/main/public/background5.jpg']
 
+  function getRandomBackground(backgrounds: any[]): any {
+    try {
+      
+      const randomIndex = Math.floor(Math.random() * backgrounds.length);
+      return backgrounds[randomIndex];
+    } catch (error) {
+      console.error('Erro ao buscar o background:', error);
+      return null;
+    }
+  }
+
+  const randomizador = getRandomBackground(backgrounds)
+  
   const fundoBundle = bundles.find((bundle) => bundle.uuid === selectedBundle)?.displayIcon ||
-  randomItem.displayIcon;
+  randomizador;
 
   const wallpaper = `url(${fundoBundle})`;
 
-  if (loading) return <div>Carregando...</div>;
+  if (loading) return 
+  <div 
+  style={{ backgroundImage: `${wallpaper}` }}
+  className="bg-cover bg-no-repeat w-screen h-screen flex flex-col justify-start items-center font-mono">
+    <div className="bg-linear-to-b from-neutral-950/50 to-neutral-950/50 w-screen h-screen flex flex-col justify-start items-center font-mono">
+      <div className="text-white text-2xl mb-4 flex flex-row justify-center items-center w-1/2 h-12 inset-x-0 top-0">
+        <div className="bg-red-400 w-14 h-12 rotate-60 translate-x-9 -translate-y-3"></div>
+        <h1 className="bg-red-400 w-1/2 flex justify-center h-12 items-center">Coleções do Valorant</h1>
+        <div className="bg-red-400 w-14 h-12 rotate-300 -translate-x-9 -translate-y-3"></div>
+      </div>
+    </div>
+  </div>;
   if (error) return <div>Erro: {error.message}</div>;
 
   return (
@@ -91,9 +111,9 @@ function App() {
             id="bundle-select"
             value={selectedBundle}
             onChange={handleBundleChange}
-            className="text-white bg-black flex flex-col justify-center items-center"
+            className="text-white/0 hover:text-white bg-neutral-950/45 flex flex-col justify-center items-center outline-none w-[17px] hover:w-80 duration-300 rounded-lg ring-red-400/25 hover:border-red-400/0 shadow-lg shadow-red-500/70 hover:shadow-red-500/0"
           >
-            <option value="" className='flex flex-col justify-center items-center'>Selecione...</option>
+            <option value="" className='flex flex-col justify-center items-center'>Selecione uma coleção</option>
             {bundles.map((bundle) => (
               <option key={bundle.displayName} value={bundle.uuid}>
                 {bundle.displayName}
@@ -115,7 +135,7 @@ function App() {
             null;
 
             if (skinIcon != null) return (
-              <div className="p-4 min-w-80 min-h-[130px] border-neutral-500 border-2 hover:border-sky-500 hover:scale-102 duration-300 rounded-lg bg-linear-to-b from-neutral-950/45 to-neutral-950/45">
+              <div className="p-4 min-w-80 min-h-[130px] border-neutral-500 border-2 hover:border-sky-500 hover:scale-102 hover:shadow-lg hover:shadow-sky-500/70 duration-300 rounded-lg bg-linear-to-b from-neutral-950/45 to-neutral-950/45">
                 <img src={skinIcon} alt="" className="max-h-24 justify-self-center"/>
               </div>
             )
